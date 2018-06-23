cbbbee7748a3fc48e1ace280834ca09f958deb64
mdeejay <mdjrussia@gmail.com>
2018-06-23 12:48:16 +0200

    whyred: Use fm script from hardware/qcom/fm

--- device.mk
+++ device.mk
@@ -220,7 +220,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FM2 \
     libqcomfm_jni \
-    qcom.fmradio
+    qcom.fmradio \
+	init.qti.fm.sh
 
 PRODUCT_BOOT_JARS += qcom.fmradio
 
@@ -362,7 +363,6 @@ PRODUCT_PACKAGES += \
 	init.qcom.syspart_fixup.sh \
 	init.qcom.usb.sh \
 	init.qcom.wifi.sh \
-	init.qti.fm.sh \
 	init.qti.ims.sh \
 	init.qti.qseecomd.sh
 
